2104e9e7225e34bc7145e68c145bf9922c90d0eb
StoicTylerDurden <buhussayyen@gmail.com>
2024-07-24 07:43:54 +0300

    Finished user auth

---
+++ config/database.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL);
+
+mongoose.connection.on('connected', () => {
+  console.info(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
